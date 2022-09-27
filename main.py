65e1bd8c871f4425768b2973c83ae1922b98c6e5
ali-murtaza-malik <ali.malik@mail.mcgill.ca>
2022-09-27 16:56:04 -0400

    My first commit!

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+    print(f"This is the start of our project. This function's name is {main_wrapper.__name__}")
+
+    #Code here
+
+    print("This is the end of our project.")
+
+if __name__ == "__main__":
+    main_wrapper()
